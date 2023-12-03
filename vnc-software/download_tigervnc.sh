8e48ae09bbee3f5c8b1346b2d35eb8f356ce7a46
Thomas Farstrike <thomasverstreken@protonmail.com>
2023-12-03 09:22:14 +0100

    Add vnc-software with downloader

---
+++ vnc-software/download_tigervnc.sh
@@ -0,0 +1,4 @@
+#wget https://netix.dl.sourceforge.net/project/tigervnc/stable/1.13.1/tigervnc-1.13.1.x86_64.tar.gz
+tar xf tigervnc-1.13.1.x86_64.tar.gz 
+rm -rf tigervnc-linux-x86_64
+mv tigervnc-1.13.1.x86_64 tigervnc-linux-x86_64
