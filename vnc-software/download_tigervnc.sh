98b304b7103e78a4e767bee3eb2d65686ec5a911
Thomas Farstrike <thomasverstreken@protonmail.com>
2023-12-03 14:25:04 +0100

    Improve download script

--- vnc-software/download_tigervnc.sh
+++ vnc-software/download_tigervnc.sh
@@ -1,4 +1,10 @@
-#wget https://netix.dl.sourceforge.net/project/tigervnc/stable/1.13.1/tigervnc-1.13.1.x86_64.tar.gz
-tar xf tigervnc-1.13.1.x86_64.tar.gz 
-rm -rf tigervnc-linux-x86_64
-mv tigervnc-1.13.1.x86_64 tigervnc-linux-x86_64
+#!/bin/sh
+
+# Download link obtained from https://sourceforge.net/projects/tigervnc/
+outfile=tigervnc-latest-stable.tar.gz
+
+wget https://netix.dl.sourceforge.net/project/tigervnc/stable/1.13.1/tigervnc-1.13.1.x86_64.tar.gz -O "$outfile" && \
+tar xf "$outfile" && \
+rm -rf tigervnc-linux-x86_64 && \
+mv tigervnc-1.13.1.x86_64 tigervnc-linux-x86_64 && \
+rm "$outfile"
