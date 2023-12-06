f382be2e12c329eeeada14f9a766888aec1d9349
Thomas Farstrike <thomasverstreken@protonmail.com>
2023-12-06 19:08:08 +0100

    Add hypertele logic
    This is an intential copy-paste of hypertele/server.js
    and hypertele/client.sh to facilitate rebasing to
    newer hypertele releases and readable diffs.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,219 @@
+// the vnc server listens on port 55900 (non-standard because another vnc server might be listening on the standard 5900)
+// the hypertele listens on port 45900,
+// so the vnc client also connects to port 45900
+
 const { app, BrowserWindow, ipcMain } = require('electron');
 import { existsSync } from 'node:fs';
+
+
+// hypertele imports:
+const HyperDHT = require('hyperdht')
+const net = require('net')
+const argv = require('minimist')(process.argv.slice(2))
+const libNet = require('@hyper-cmd/lib-net')
+const libUtils = require('@hyper-cmd/lib-utils')
+const libKeys = require('@hyper-cmd/lib-keys')
+const connPiper = libNet.connPiper
+
+
+function startHyperTeleServer() {
+
+argv.l = "55900";
+argv.seed = libKeys.randomBytes(32).toString('hex');
+
+const helpMsg = 'Usage:\nhypertele-server -l service_port -u unix_socket ?--address service_address ?-c conf.json ?--seed seed ?--cert-skip'
+
+if (argv.help) {
+  console.log(helpMsg)
+  process.exit(-1)
+}
+
+if (!argv.u && !+argv.l) {
+  console.error('Error: proxy port invalid')
+  process.exit(-1)
+}
+
+if (argv.u && argv.l) {
+  console.error('Error: cannot listen to both a port and a Unix domain socket')
+  process.exit(-1)
+}
+
+const conf = {}
+
+if (argv.seed) {
+  conf.seed = argv.seed
+}
+
+if (argv.c) {
+  libUtils.readConf(conf, argv.c)
+}
+
+if (argv.compress) {
+  conf.compress = true
+}
+
+if (argv['cert-skip']) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+}
+
+if (!conf.seed) {
+  console.error('Error: conf.seed invalid')
+  process.exit(-1)
+}
+
+if (conf.allow) {
+  conf.allow = libKeys.prepKeyList(conf.allow)
+}
+
+const debug = argv.debug
+
+const seed = Buffer.from(conf.seed, 'hex')
+
+const dht = new HyperDHT()
+const keyPair = HyperDHT.keyPair(seed)
+
+const stats = {}
+
+const destIp = argv.address || '127.0.0.1'
+
+const server = dht.createServer({
+  firewall: (remotePublicKey, remoteHandshakePayload) => {
+    if (conf.allow && !libKeys.checkAllowList(conf.allow, remotePublicKey)) {
+      return true
+    }
+
+    return false
+  },
+  reusableSocket: true
+}, c => {
+  connPiper(c, () => {
+    return net.connect(
+      argv.u
+        ? { path: argv.u }
+        : { port: +argv.l, host: destIp, allowHalfOpen: true }
+    )
+  }, { debug, isServer: true, compress: conf.compress }, stats)
+})
+
+server.listen(keyPair).then(() => {
+  console.log('hypertele:', keyPair.publicKey.toString('hex'))
+})
+
+if (debug) {
+  setInterval(() => {
+    console.log('connection stats', stats)
+  }, 5000)
+}
+
+process.once('SIGINT', function () {
+  dht.destroy()
+})
+
+return keyPair.publicKey.toString('hex');
+
+}
+
+
+
+
+
+function startHyperTeleClient(pubkeyhex) {
+
+argv.s = pubkeyhex;
+argv.p = "45900";
+
+const helpMsg = 'Usage:\nhypertele -p port_listen -u unix_socket ?-c conf.json ?-i identity.json ?-s peer_key'
+
+if (argv.help) {
+  console.log(helpMsg)
+  process.exit(-1)
+}
+
+if (!argv.u && !+argv.p) {
+  console.error('Error: proxy port invalid')
+  process.exit(-1)
+}
+
+if (argv.u && argv.p) {
+  console.error('Error: cannot listen to both a port and a Unix domain socket')
+  process.exit(-1)
+}
+const conf = {}
+
+const target = argv.u ? argv.u : +argv.p
+
+if (argv.s) {
+  conf.peer = libUtils.resolveHostToKey([], argv.s)
+}
+
+if (argv.c) {
+  libUtils.readConf(conf, argv.c)
+}
+
+if (!conf.keepAlive) {
+  conf.keepAlive = 5000
+}
+
+if (argv.compress) {
+  conf.compress = true
+}
+
+const peer = conf.peer
+if (!peer) {
+  console.error('Error: peer is invalid')
+  process.exit(-1)
+}
+
+const debug = argv.debug
+
+let keyPair = null
+if (argv.i) {
+  keyPair = libUtils.resolveIdentity([], argv.i)
+
+  if (!keyPair) {
+    console.error('Error: identity file invalid')
+    process.exit(-1)
+  }
+
+  keyPair = libKeys.parseKeyPair(keyPair)
+}
+
+const stats = {}
+
+const dht = new HyperDHT({
+  keyPair
+})
+
+const proxy = net.createServer({ allowHalfOpen: true }, c => {
+  return connPiper(c, () => {
+    const stream = dht.connect(Buffer.from(peer, 'hex'), { reusableSocket: true })
+    stream.setKeepAlive(conf.keepAlive)
+
+    return stream
+  }, { compress: conf.compress }, stats)
+})
+
+if (debug) {
+  setInterval(() => {
+    console.log('connection stats', stats)
+  }, 5000)
+}
+
+proxy.listen(target, () => {
+  console.log(`Server ready @${target}`)
+})
+
+process.once('SIGINT', () => {
+  dht.destroy().then(() => {
+    process.exit()
+  })
+})
+
+}
+
+
+
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -49,7 +263,8 @@ app.on('activate', () => {
 
 // Depending on how the app is launched, the tigervnc folder might be in different locations.
 // Search order:
-// - relative tigervnc/tigervnc-1.13.1.x86_64/usr/bin/x0vncserver
+// - vnc-software/ (when launching in development)
+// - /tmp/.mount_peervizdlxvC/resources/vnc-software/ (launched from appimage or .deb package installation)
 function findResourceFile(filename) {
 	console.log(__dirname)
 	console.log(process.cwd())
@@ -74,18 +289,22 @@ function findResourceFile(filename) {
 }
 
 ipcMain.on('run-server', (event) => {
+  let publicKeyHex = startHyperTeleServer();
+  event.reply('run-server-pubkey', 'publicKeyHex: ' + publicKeyHex);
   // default debian package has x0tigervncserver instead
-  let result = runProcess('tigervnc-linux-x86_64/usr/bin/x0vncserver');
+  let result = runProcess('tigervnc-linux-x86_64/usr/bin/x0vncserver', '-localhost=1 -interface=127.0.0.1 -rfbport 55900');
   event.reply('run-server-result', 'running server result: ' + result);
 });
 
-ipcMain.on('run-client', (event) => {
+ipcMain.on('run-client', (event, data) => {
+  console.log("running client with connectto data: " + data);
+  startHyperTeleClient(data);
   // default debian package has xtigervncviewer instead
-  let result = runProcess('tigervnc-linux-x86_64/usr/bin/vncviewer');
+  let result = runProcess('tigervnc-linux-x86_64/usr/bin/vncviewer', '127.0.0.1::45900');
   event.reply('run-client-result', 'running client result: ' + result);
 });
 
-function runProcess(binaryName) {
+function runProcess(binaryName, args) {
   if (process.platform !== 'linux') { // win32/darwin not supported
     console.log('Unsupported platform ' + process.platform + '. Only linux is supported, win32/darwin not yet. Please reach out!');
     return -1;
@@ -97,7 +316,7 @@ function runProcess(binaryName) {
     return -2;
   }
 
-  let command = foundBinary + " -SecurityTypes None";
+  let command = foundBinary + " -SecurityTypes None" + " " + args;
   console.log("Doing exec of: " + command);
   const { exec } = require('child_process');
   exec(command, (error, stdout, stderr) => {
