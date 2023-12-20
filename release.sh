4f0f5f767bd21c3614b668773f2a80f63965bfd9
Thomas Farstrike <thomasverstreken@protonmail.com>
2023-12-20 21:20:08 +0100

    Improve release script

--- release.sh
+++ release.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
-echo "1) make sure devtools are disabled in src/main.js"
+echo "0) make sure all changes are committed"
+diff=$(git diff | wc -l)
+if [ $diff -gt 0 ]; then
+	echo "You have diff"
+	exit 1
+fi
+
+echo "1) make sure devtools are set to false in src/main.js"
 read yes
 
 echo "2) make sure you've incremented the version number in package.json"
 read yes
 
-echo "3) Running: electron-forge package"
+echo "3) Cleaning up old build..."
+rm -rf .webpack/
+rm -rf dist/
+
+echo "4) Running: electron-forge package"
 ./node_modules/.bin/electron-forge package
 
-echo "4) Running electron-builder -l appimage zip deb snap rpm"
+echo "5) Running electron-builder -l appimage zip deb snap rpm"
 export DEBUG=electron-builder
 ./node_modules/.bin/electron-builder -l appimage zip deb snap rpm
 
