fb622fe29b8c20ed4a322a988ad706f3762341cc
Thomas Farstrike <thomasverstreken@protonmail.com>
2024-01-22 11:13:57 +0100

    Add comments

--- release.sh
+++ release.sh
@@ -17,9 +17,17 @@ echo "3) Cleaning up old build..."
 rm -rf .webpack/
 rm -rf dist/
 
+# If you run it in dev mode first with:
+# npm run start
+# Then the app tries to connect to localhost:3000 instead of packaging the actual files when building with:
+#./node_modules/.bin/electron-builder
+
+# Therefore, first make sure the correct files are packaged with electron-forge:
 echo "4) Running: electron-forge package"
 ./node_modules/.bin/electron-forge package
 
+# NOTE: Don't use prepackage because the electron-builder will not add the tigervnc/ folder:
+#./node_modules/.bin/electron-builder --prepackaged  out/peerviewer-linux-x64/ -l appimage
 echo "5) Running electron-builder -l appimage zip deb snap rpm"
 export DEBUG=electron-builder
 ./node_modules/.bin/electron-builder -l appimage zip deb snap rpm
