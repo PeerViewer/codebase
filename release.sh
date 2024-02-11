c7e43f690597a1bcd529cc38526277cf53035c1a
Thomas Farstrike <thomasverstreken@protonmail.com>
2024-02-11 10:17:30 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -30,9 +30,18 @@ echo "4) Running: electron-forge package"
 #./node_modules/.bin/electron-builder --prepackaged  out/peerviewer-linux-x64/ -l appimage
 echo "5) Running electron-builder --linux appimage zip deb snap rpm"
 export DEBUG=electron-builder
-./node_modules/.bin/electron-builder --linux appimage zip deb snap rpm 
+./node_modules/.bin/electron-builder --linux appimage deb snap rpm tar.gz
 
-echo "6) Running electron-builder --win"
-# This requires wine32, see https://www.electron.build/multi-platform-build so something like:
+# Building for Windows on Linux doesn't seem to work, see:
+# https://github.com/electron/electron/issues/1075
+# https://github.com/electron-userland/electron-builder/issues/844
+# https://github.com/electron-userland/electron-builder/issues/5254
+#
+# It also requires wine32, see https://www.electron.build/multi-platform-build so something like:
 # dpkg --add-architecture i386 && apt-get update && apt-get install wine32
-./node_modules/.bin/electron-builder --win
+#./node_modules/.bin/electron-builder --win nsis portable msi zip
+#
+# To build for Windows, execute on a Windows machine (as Administrator, otherwise symlink permission errors):
+# node_modules\\.bin\\electron-builder --win nsis portable msi zip
+
+
